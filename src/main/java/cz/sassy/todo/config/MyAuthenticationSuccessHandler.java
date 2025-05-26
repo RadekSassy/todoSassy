2323c3a36841f844a171590e107abfeef2631447
RadekSassy <radek@gradepoint.cz>
2025-05-26 08:56:43 +0200

    Add Javadoc comments to MyAuthenticationSuccessHandler for improved documentation

--- src/main/java/cz/sassy/todo/config/MyAuthenticationSuccessHandler.java
+++ src/main/java/cz/sassy/todo/config/MyAuthenticationSuccessHandler.java
@@ -13,8 +13,18 @@ import java.io.IOException;
  * SavedRequestAwareAuthenticationSuccessHandler. It determines the target URL after successful authentication
  * based on the user's roles.
  */
-
 public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    /**
+     * This method is called when authentication is successful.
+     * It checks if the authenticated user has the "ROLE_ADMIN" authority and sets the default target URL accordingly.
+     *
+     * @param request        the HttpServletRequest object
+     * @param response       the HttpServletResponse object
+     * @param authentication the Authentication object containing user details
+     * @throws ServletException if an error occurs during processing
+     * @throws IOException      if an I/O error occurs
+     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
 
